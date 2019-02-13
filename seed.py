eb3f2e7d2c7e2e13c43a68a234beb2bfb0475b4a
vagrant <vagrant@vagrant.vm>
2019-02-13 01:06:55 +0000

    Added templates

--- seed.py
+++ seed.py
@@ -27,28 +27,28 @@ def load_books():
         db.session.add(book)
         db.session.commit()
 
-def load_users():
-    """Load users from u.user into database."""
+# def load_users():
+#     """Load users from u.user into database."""
 
-    print("Users")
+#     print("Users")
 
-    # ©Delete all rows in table, so if we need to run this a second time,
-    # we won't be trying to add duplicate users
-    User.query.delete()
+#     # ©Delete all rows in table, so if we need to run this a second time,
+#     # we won't be trying to add duplicate users
+#     User.query.delete()
 
-    # Read u.user file and insert data
-    for row in open("u.user"):
-        row = row.rstrip()
-        user_id, age, gender, occupation, zipcode = row.split("|")
-        print(zipcode)
-        user = User(user_id=user_id,
-                    age=age)
+#     # Read u.user file and insert data
+#     for row in open("u.user"):
+#         row = row.rstrip()
+#         user_id, age, gender, occupation, zipcode = row.split("|")
+#         print(zipcode)
+#         user = User(user_id=user_id,
+#                     age=age)
 
-        # We need to add to the session or it won't ever be stored
-        db.session.add(user)
+#         # We need to add to the session or it won't ever be stored
+#         db.session.add(user)
 
-    # Once we're done, we should commit our work
-    db.session.commit()
+#     # Once we're done, we should commit our work
+#     db.session.commit()
             
         
 
--- server.py
+++ server.py
@@ -9,3 +9,80 @@ from model import User, Book_shelf, Book, connect_to_db, db
 
 
 app = Flask(__name__)
+
+app.secret_key = "ABC"
+
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/')
+def indenx():
+    """Homepage"""
+
+    return render_template("homepage.html")
+
+
+@app.route('/register', methods=["POST"])
+def register_process():
+    """Add user information to database"""
+
+    email = request.form["email"]
+    password = request.form['password']
+
+    try:
+        User.query.filter(User.email == email ).one()
+    
+    except:
+        user = User(email=email,
+                    password=password)
+        
+        db.session.add(user)
+        db.session.commit()
+
+    return redirect("/")
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process user login"""
+
+    email = request.form['email']
+    password = request.form['password']
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("Email not recognized, please try again.")
+        return redirect('/login')
+
+    if user.password != password:
+        flash("Incorrect password, try again")
+        return redirect('/login')
+
+    session['user_id']  = user.user_id
+
+    flash('Logged in')
+
+@app.route('/logout')
+def logout():
+    """Log user out"""
+
+    del session['user_id']
+    flash('Logged out')
+    return redirect('/')
+
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
